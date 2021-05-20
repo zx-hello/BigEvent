ec2b248671cd69a6acfb2eff655a73ea9aeaccc5
zx-hello <18330618963@163.com>
2021-05-20 16:07:37 +0800

    个人中心中 基本资料完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,25 +22,29 @@
 // )
 
 // -------发送请求 获取用户信息
-axios.get('/my/user/userinfo').then(res => {
-  // console.log(res)
-  // 解构  看清楚结构！！！！ res.data.data
-  let {nickname, user_pic, username} = res.data.data
-  // 设置欢迎 名称  若用户有昵称就使用 没有就使用username
-  let name = nickname || username
-  $('.username').text(name)
-  // 显示头像 新用户没有头像 使用name的第一个字符  当有头像时 使用头像图片
-  if (user_pic) {
-    // 说明有头像
-    $('.layui-nav-img').attr('src', user_pic).show()
-  } else {
-    // 说明没有头像  截取字符串三种方式  substr substring splice
-    // 截取第一个字符串当作头像  若是字母转成大写
-    let first = name.substring(0, 1).toUpperCase()
-    // 此处使用show()方法 会显示元素本身的display状态 所以不适合使用show()
-    $('.user-avatar').text(first).css('display', 'inline-block')
-  }
-})
+function getUserInfo() {
+  axios.get('/my/user/userinfo').then(res => {
+    // console.log(res)
+    // 解构  看清楚结构！！！！ res.data.data
+    let {nickname, user_pic, username} = res.data.data
+    // 设置欢迎 名称  若用户有昵称就使用 没有就使用username
+    let name = nickname || username
+    $('.username').text(name)
+    // 显示头像 新用户没有头像 使用name的第一个字符  当有头像时 使用头像图片
+    if (user_pic) {
+      // 说明有头像
+      $('.layui-nav-img').attr('src', user_pic).show()
+    } else {
+      // 说明没有头像  截取字符串三种方式  substr substring splice
+      // 截取第一个字符串当作头像  若是字母转成大写
+      let first = name.substring(0, 1).toUpperCase()
+      // 此处使用show()方法 会显示元素本身的display状态 所以不适合使用show()
+      $('.user-avatar').text(first).css('display', 'inline-block')
+    }
+  })
+}
+
+getUserInfo()
 
 // ---------退出
 $('#logout').on('click', function () {
---
+++ user/js/userinfo.js
@@ -0,0 +1,45 @@
+// --------数据回填
+// 只要是修改操作 全都需要数据回填
+// 数据回填 就是获取到原来的数据 设置输入框的默认值即可
+function renderUser() {
+  // 获取用户数据  设置输入框默认值
+  axios.get('/my/user/userinfo').then(res => {
+    // console.log(res)
+    // let {username, nickname, email} = res.data.data
+    // $('input[name=username]').val(username)
+    // $('input[name=nickname]').val(nickname)
+    // $('input[name=email]').val(email)
+    
+    // 优化  使用layui提供的form.val()进行数据回填
+    let form = layui.form
+    // form.val('表单 lay-filter 属性值', '{数据} 对象格式 键要等于表单的name属性')
+    form.val('abc', res.data.data)
+  })
+}
+
+renderUser()
+
+// -----------修改昵称和邮箱
+$('form').on('submit', function (e) {
+  e.preventDefault()
+  let data = $(this).serialize()
+  axios.post('/my/user/userinfo', data).then(res => {
+    // console.log(res)
+    let {status, message} = res.data
+    if (status === 0) {
+      layer.msg(message)
+      // 更新欢迎语句的名称
+      // 调用父页面的函数 必须有两个条件
+      // 1.使用iframe把两个页面联系起来 形成父子页面的结构
+      // 2.必须在真是的服务器环境才可以使用 live server
+      window.parent.getUserInfo()
+    }
+  })
+})
+
+// -----------重置按钮
+// 点击重置按钮 不会清空所有输入框 而是恢复到修改前的数据
+$('button[type=reset]').on('click', function (e) {
+  e.preventDefault()
+  renderUser()
+})
